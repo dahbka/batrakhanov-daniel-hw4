c7776185e6111e96273f0c42b7ad7f80165029e2
dahbka <dahbka96@gmail.com>
2020-04-03 16:10:46 +0500

    stepik template input

--- app/src/test/java/com/example/batrakhanov_daniel_hw4/ExampleUnitTest.kt
+++ app/src/test/java/com/example/batrakhanov_daniel_hw4/ExampleUnitTest.kt
@@ -10,8 +10,41 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun example() {
+
+        val iphoneCase = Product(price = 123.5, salePercent = 30)
+
+        val pricePrinter: PricePrinter = <...>
+
+        val discountIphoneCasePrice = iphoneCase.calcDiscountPrice()
+        pricePrinter.print(discountIphoneCasePrice)
     }
+}
+
+class Product(
+    /**
+     * Must be positive
+     */
+    private val price: Double,
+    private val salePercent: Int = 0
+) {
+    /**
+     * @return price with applied discount determined by [salePercent]
+     *
+     * If [salePercent] is more than 100 than product will have negative price
+     * If [salePercent] less than 0 product price will be increased
+     */
+    fun calcDiscountPrice(): Double = price * (1 - salePercent / 100.0)
+}
+
+interface PricePrinter {
+
+    /**
+     * Outputs price in <PRICE>P format.
+     * If price have not fractional part than it will be printed as integer
+     * If price have fractional part than it will be rounded for 2 symbols after "."
+     */
+    fun print(price: Double)
 }
